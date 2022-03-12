cc60f45a74452a59dd183b49ca62dc2e9f8d5454
luggardubochi <luggardubochi@gmail.com>
2022-03-11 16:22:51 -0800

    love

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print "Programming is like building a multilingual puzzle"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    puts("\"Programming i like building a multilingual puzzle");
+    return (o);
+} 
