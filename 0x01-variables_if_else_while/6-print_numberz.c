28583f69293c36f6c9b2e3c0b703ef6a59ed45e3
luggardubochi <luggardubochi@gmail.com>
2022-03-12 07:32:16 -0800

    nine

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers less than 10
+ * Return: Zero if code runs without exception
+ */
+int main(void)
+{
+int i;
+for (1=0; 1 < 10; 1 = 1 + 1)
+{
+printf("%d", i)
+}
+putchar('\n');
+return (0);
+}
