0e05ea9c3fadad82825ddd0de439182cc097838b
luggardubochi <luggardubochi@gmail.com>
2022-03-13 09:57:56 -0700

    fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * print the alphabet in reverse order
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int i:
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
