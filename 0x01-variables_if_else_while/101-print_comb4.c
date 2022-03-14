7c649a1cbec24b44cf9927ae92e5b6126bd88b7c
luggardubochi <luggardubochi@gmail.com>
2022-03-14 13:24:30 -0700

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k, l;
+for (i = 0; i < 1000; i++)
+{
+j = i / 100;
+k = (i / 10) % 10;
+l = i % 10;
+if (j < k && k < l)
+{
+putchar(j + '0');
+putchar(k + '0');
+putchar(l + '0');
+if (i < 700)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
