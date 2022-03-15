b7c6f07a2542de6f5e69f9406800df347b153886
luggardubochi <luggardubochi@gmail.com>
2022-03-15 07:12:52 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - print the alphabeth in lowercase followed by a new line
+ * Return: zero (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+return (0);
+}
