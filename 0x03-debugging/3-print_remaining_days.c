972fcd98b5dace975604ca86287a719b239fbf81
luggardubochi <luggardubochi@gmail.com>
2022-03-17 23:23:53 -0700

    done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,8 +24,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
